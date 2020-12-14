2f84ef7c37211120fd14992241e12156e6b7a207
joel <joelcrawford30@gmail.com>
2020-12-14 23:26:43 +0300

    first commit

---
+++ app/src/main/java/com/example/kotlinyoutubelbta/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.kotlinyoutubelbta
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rCV: RecyclerView = findViewById<RecyclerView>(R.id.recyclerView_main)
+
+
+        rCV.setBackgroundColor(Color.BLUE)
+    }
+}
