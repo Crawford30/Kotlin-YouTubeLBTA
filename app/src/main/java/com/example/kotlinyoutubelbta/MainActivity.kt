7c98cece7669bc486eca633e73fe39381918e765
joel <joelcrawford30@gmail.com>
2020-12-15 13:34:28 +0300

    Model file

--- app/src/main/java/com/example/kotlinyoutubelbta/MainActivity.kt
+++ app/src/main/java/com/example/kotlinyoutubelbta/MainActivity.kt
@@ -83,22 +83,22 @@ class MainActivity : AppCompatActivity() {
 }
 
 
-//"id": 3,
-//"name": "Kindle Basic Training",
-//"link": "https://www.letsbuildthatapp.com/basic-training",
-//"imageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/114bec2f-fbfd-4b13-91de-907fe57c6e37",
-//"numberOfViews": 7500,
-//"channel": {
-//    "name": "Lets Build That App",
-//    "profileImageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/dda5bc77-327f-4944-8f51-ba4f3651ffdf",
-//    "numberOfSubscribers": 100000
-//}
-
-
-//class model for data
-class HomeFeed(val videos: List<Video>)
-
-class Video(val id: Int, val name: String, val link:String, val imageURL:String, val numberOfViews: Int,
-val channel: Channel)
-
-class Channel(val name: String, val profileImageUrl:String)
+////"id": 3,
+////"name": "Kindle Basic Training",
+////"link": "https://www.letsbuildthatapp.com/basic-training",
+////"imageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/114bec2f-fbfd-4b13-91de-907fe57c6e37",
+////"numberOfViews": 7500,
+////"channel": {
+////    "name": "Lets Build That App",
+////    "profileImageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/dda5bc77-327f-4944-8f51-ba4f3651ffdf",
+////    "numberOfSubscribers": 100000
+////}
+//
+//
+////class model for data
+//class HomeFeed(val videos: List<Video>)
+//
+//class Video(val id: Int, val name: String, val link:String, val imageURL:String, val numberOfViews: Int,
+//val channel: Channel)
+//
+//class Channel(val name: String, val profileImageUrl:String)
